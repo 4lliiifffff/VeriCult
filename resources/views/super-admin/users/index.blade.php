7a5383528323851dc064274208a66d8027adddee
Muhamad Alif Nur Rohman <alifnurrahman29@gmail.com>
2026-05-09 17:53:25 +0700

    Fix hover

--- resources/views/super-admin/users/index.blade.php
+++ resources/views/super-admin/users/index.blade.php
@@ -7,7 +7,7 @@
             <span class="text-[#03045E]">Kelola Pengguna</span>
         </nav>
 
-        <div class="relative bg-white rounded-[2rem] sm:rounded-[2.5rem] p-6 sm:p-10 shadow-xl shadow-slate-200/100 border border-slate-100 overflow-hidden group">
+        <div class="relative bg-white rounded-[2rem] sm:rounded-[2.5rem] p-6 sm:p-10 shadow-xl shadow-slate-200/100 border border-slate-100 overflow-hidden">
             <div class="absolute top-0 right-0 -mt-20 -mr-20 w-96 h-96 bg-blue-50/50 rounded-full transition-transform duration-1000 group-hover:scale-110"></div>
             
             <div class="relative z-10 flex flex-col md:flex-row md:items-center justify-between gap-6 sm:gap-8">
