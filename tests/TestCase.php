288b3a750c84d57dbf515d07ac47e8a931a3fb8b
layangpuspa <lay.431puspa@gmail.com>
2026-05-12 10:12:18 +0700

    Pelaporan yang dapat diunduh oleh publik

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,20 @@
 
 namespace Tests;
 
+use Database\Seeders\RoleSeeder;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Spatie\Permission\PermissionRegistrar;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        app(PermissionRegistrar::class)->forgetCachedPermissions();
+
+        if (app()->environment('testing')) {
+            $this->seed(RoleSeeder::class);
+        }
+    }
 }
